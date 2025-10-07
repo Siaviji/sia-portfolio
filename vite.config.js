d867fc9d8924735bbb198d50c9f2a7e63edf061c
Siaviji <siaviji2002@gmail.com>
2025-10-07 14:53:24 -0400

    Initial portfolio

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-export default defineConfig({ plugins:[react()], base:'/REPO_NAME/' });
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/sia-portfolio/',   // <-- must match the repo name
+})
